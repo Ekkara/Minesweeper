c348e901b6b1bf356e5a4ef0d561d9628a87add7
Ekkara <47211128+Ekkara@users.noreply.github.com>
2022-07-08 03:37:23 +0200

    AI helper
    a very easy and unoptmized implentation of the assisting AI. future optimize, coment, clean up and add more visual representation

--- MineSweeper/Assets/MapGenerator.cs
+++ MineSweeper/Assets/MapGenerator.cs
@@ -6,20 +6,26 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] [Range(9, 100)] int width = 18, height = 14;
     [SerializeField] int mines = 10;
     [SerializeField] GameObject tile;
-    [SerializeField] Color color1, color2, BGColor1, BGColor2, flagColor1, flagColor2;
+    [SerializeField] Color color1, color2, BGColor1, BGColor2, hintColor1, hintColor2, flagColor1, flagColor2;
     [SerializeField] GameObject flag;
     [SerializeField] GameObject numberOfColidingMines;
     [SerializeField] Color mine1, mine2, mine3, mine4, mine5, mine6, mine7, mine8;
     [SerializeField] Tile[,] mineField;
     [HideInInspector] [SerializeField] bool mapMade = false;
+    bool lost;
+
+    private void Start() {
+        lost = false;
+        GenerateTiles();
+    }
 
     private void OnValidate() {
-        if (mines >= ((width * height)-9)) {
+        if (mines >= ((width * height) - 9)) {
             int newAmount = ((width * height)) - 9;
             Debug.LogWarning("mines can't cover the entire field, value changed to entire field minus nine, the new value is: " + newAmount);
             mines = newAmount;
         }
-        if(numberOfColidingMines.GetComponent<TextMesh>() == null) {
+        if (numberOfColidingMines.GetComponent<TextMesh>() == null) {
             Debug.LogWarning("objectDidn't contain text");
             numberOfColidingMines.AddComponent<TextMesh>();
         }
@@ -59,10 +65,10 @@ public class MapGenerator : MonoBehaviour
         mapMade = true;
     }
     void LeftClick(int wPos, int hPos) {
-        if (mineField[wPos, hPos].state.Equals(0) && !mineField[wPos, hPos].isPressed) {           
+        if (mineField[wPos, hPos].state.Equals(0) && !mineField[wPos, hPos].isPressed) {
             if (mineField[wPos, hPos].containMine) {
                 mineField[wPos, hPos].tileGO.GetComponent<SpriteRenderer>().color = Color.red;
-                GenerateTiles();
+                lost = true;
             }
             else {
                 UpdateVision(wPos, hPos);
@@ -72,7 +78,7 @@ public class MapGenerator : MonoBehaviour
     void UpdateVision(int wp, int hp) {
         List<Arr> remainingPos = new List<Arr>();
         remainingPos.Add(new Arr(wp, hp));
-        while(remainingPos.Count > 0) {
+        while (remainingPos.Count > 0) {
             if (mineField[remainingPos[0].w, remainingPos[0].h].isPressed) {
                 remainingPos.RemoveAt(0);
                 continue;
@@ -128,7 +134,7 @@ public class MapGenerator : MonoBehaviour
             remainingPos.RemoveAt(0);
             currentFound++;
         }
-        if(currentFound >= minSafeFound) {
+        if (currentFound >= minSafeFound) {
             Debug.Log("Win");
         }
     }
@@ -137,13 +143,13 @@ public class MapGenerator : MonoBehaviour
             return;
 
         GameObject newNumber = Instantiate(numberOfColidingMines);
-        newNumber.transform.position = new Vector3(w,h,numberOfColidingMines.transform.position.z);
+        newNumber.transform.position = new Vector3(w, h, numberOfColidingMines.transform.position.z);
         newNumber.transform.SetParent(mineField[w, h].tileGO.transform);
         TextMesh text = newNumber.GetComponent<TextMesh>();
         text.text = mineField[w, h].colidingMineAmount.ToString();
-        switch(mineField[w, h].colidingMineAmount){
+        switch (mineField[w, h].colidingMineAmount) {
             case (1):
-            text.color = mine1;
+                text.color = mine1;
                 break;
             case (2):
                 text.color = mine2;
@@ -205,31 +211,31 @@ public class MapGenerator : MonoBehaviour
         //remove first mine to avoid sudden death at start as well as all adjecent tiles, to avoid starting on a number
         for (int w = 0; w < width; w++) {
             for (int h = 0; h < height; h++) {
-                if (w.Equals(wPos) && h.Equals(hPos+1)) {
+                if (w.Equals(wPos) && h.Equals(hPos + 1)) {
                     continue;
                 }
-                if (w.Equals(wPos) && h.Equals(hPos-1)) {
+                if (w.Equals(wPos) && h.Equals(hPos - 1)) {
                     continue;
                 }
                 if (w.Equals(wPos) && h.Equals(hPos)) {
                     continue;
                 }
-                if (w.Equals(wPos+1) && h.Equals(hPos+1)) {
+                if (w.Equals(wPos + 1) && h.Equals(hPos + 1)) {
                     continue;
                 }
-                if (w.Equals(wPos+1) && h.Equals(hPos-1)) {
+                if (w.Equals(wPos + 1) && h.Equals(hPos - 1)) {
                     continue;
                 }
-                if (w.Equals(wPos+1) && h.Equals(hPos)) {
+                if (w.Equals(wPos + 1) && h.Equals(hPos)) {
                     continue;
                 }
-                if (w.Equals(wPos-1) && h.Equals(hPos+1)) {
+                if (w.Equals(wPos - 1) && h.Equals(hPos + 1)) {
                     continue;
                 }
-                if (w.Equals(wPos-1) && h.Equals(hPos-1)) {
+                if (w.Equals(wPos - 1) && h.Equals(hPos - 1)) {
                     continue;
                 }
-                if (w.Equals(wPos-1) && h.Equals(hPos)) {
+                if (w.Equals(wPos - 1) && h.Equals(hPos)) {
                     continue;
                 }
 
@@ -277,8 +283,7 @@ public class MapGenerator : MonoBehaviour
                         }
                     }
                     if (h < height - 1) {
-                        if (mineField[w + 1, h + 1].containMine) 
-                        {
+                        if (mineField[w + 1, h + 1].containMine) {
                             countedMines++;
                         }
                     }
@@ -289,8 +294,7 @@ public class MapGenerator : MonoBehaviour
                     }
                 }
                 if (h < height - 1) {
-                    if (mineField[w, h + 1].containMine) 
-                        {
+                    if (mineField[w, h + 1].containMine) {
                         countedMines++;
                     }
                 }
@@ -301,16 +305,18 @@ public class MapGenerator : MonoBehaviour
     bool firstPress = true;
     int minSafeFound, currentFound;
 
-    
+
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            LayerMask mask = LayerMask.GetMask("Tile");
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, mask);
-            if (hit.collider != null) {
-                Tile tile = mineField[(int)hit.transform.position.x, (int)hit.transform.position.y];
-                Debug.Log(((int)tile.tileGO.transform.position.x).ToString() + ":" + ((int)tile.tileGO.transform.position.y).ToString());
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.R)) {
+            if (lost) {
+                GenerateTiles();
+                lost = false;
             }
+        }
+        if (lost)
+            return;
+        if (Input.GetKeyDown(KeyCode.H)) {
+            GenerateClue();
         }
         if (Input.GetMouseButtonDown(0)) {
             LayerMask mask = LayerMask.GetMask("Tile");
@@ -337,6 +343,207 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
+    
+    Arr currentHint;
+    void GenerateClue() {
+        List<Arr> possibleHints = new List<Arr>();//list of possible squares
+        List<Arr> hidenFlags = new List<Arr>();//list of safe tiles that the player might not know of
+
+        //if it is not empty calculate all possible tiles
+        #region easy check
+
+        for (int w = 0; w < width; w++) {
+            for (int h = 0; h < height; h++) {
+                if (!mineField[w, h].isPressed)
+                    continue;
+
+                int flagedTiles = 0;
+                int unkownTiles = 0;
+                int amountOBombs = mineField[w, h].colidingMineAmount;
+                List<Arr> potentialBombs = new List<Arr>();
+                if (w >= 1) {
+                    if (!mineField[w - 1, h].isPressed && mineField[w - 1, h].state == 0) {
+                        unkownTiles++;
+                        potentialBombs.Add(new Arr(w - 1, h));
+                    }
+                    if (mineField[w - 1, h].state > 0) {
+                        flagedTiles++;
+                    }
+                    if (h >= 1) {
+                        if (!mineField[w - 1, h - 1].isPressed && mineField[w - 1, h - 1].state == 0) {
+                            unkownTiles++;
+                            potentialBombs.Add(new Arr(w - 1, h - 1));
+                        }
+                        if (mineField[w - 1, h - 1].state > 0) {
+                            flagedTiles++;
+                        }
+                    }
+                    if (h < height - 1) {
+                        if (!mineField[w - 1, h + 1].isPressed && mineField[w - 1, h + 1].state == 0) {
+                            potentialBombs.Add(new Arr(w - 1, h + 1));
+                            unkownTiles++;
+                        }
+                        if (mineField[w - 1, h + 1].state > 0) {
+                            flagedTiles++;
+                        }
+                    }
+                }
+                if (w < width - 1) {
+                    if (!mineField[w + 1, h].isPressed && mineField[w + 1, h].state == 0) {
+                        potentialBombs.Add(new Arr(w + 1, h));
+                        unkownTiles++;
+                    }
+                    if (mineField[w + 1, h].state > 0) {
+                        flagedTiles++;
+                    }
+                    if (h >= 1) {
+                        if (!mineField[w + 1, h - 1].isPressed && mineField[w + 1, h - 1].state == 0) {
+                            potentialBombs.Add(new Arr(w + 1, h - 1));
+                            unkownTiles++;
+                        }
+                        if (mineField[w + 1, h - 1].state > 0) {
+                            flagedTiles++;
+                        }
+                    }
+                    if (h < height - 1) {
+                        if (!mineField[w + 1, h + 1].isPressed && mineField[w + 1, h + 1].state == 0) {
+                            potentialBombs.Add(new Arr(w + 1, h + 1));
+                            unkownTiles++;
+                        }
+                        if (mineField[w + 1, h + 1].state > 0) {
+                            flagedTiles++;
+                        }
+                    }
+                }
+                if (h >= 1) {
+                    if (!mineField[w, h - 1].isPressed && mineField[w, h - 1].state == 0) {
+                        potentialBombs.Add(new Arr(w, h - 1));
+                        unkownTiles++;
+                    }
+                    if (mineField[w, h - 1].state > 0) {
+                        flagedTiles++;
+                    }
+                }
+                if (h < height - 1) {
+                    if (!mineField[w, h + 1].isPressed && mineField[w, h + 1].state == 0) {
+                        potentialBombs.Add(new Arr(w, h + 1));
+                        unkownTiles++;
+                    }
+                    if (mineField[w, h + 1].state > 0) {
+                        flagedTiles++;
+                    }
+                }
+                 if ((amountOBombs - flagedTiles).Equals(unkownTiles)) {
+                     foreach (Arr ar in potentialBombs) {
+                         hidenFlags.Add(ar);
+                         mineField[ar.w, ar.h].state = 3;
+                     }
+                 }
+            }
+        }
+
+        //look for bombs that player have missed
+        for (int w = 0; w < width; w++) {
+            for (int h = 0; h < height; h++) {
+                if (!mineField[w, h].isPressed)
+                    continue;
+
+                int flagedTiles = 0;
+                int amountOBombs = mineField[w, h].colidingMineAmount;
+                List<Arr> potentialBombs = new List<Arr>();
+                if (w >= 1) {
+                    if (!mineField[w - 1, h].isPressed && mineField[w - 1, h].state == 0) {
+                         potentialBombs.Add(new Arr(w - 1, h));
+                    }
+                    if (mineField[w - 1, h].state > 0) {
+                        flagedTiles++;
+                    }
+                    if (h >= 1) {
+                        if (!mineField[w - 1, h - 1].isPressed && mineField[w - 1, h - 1].state == 0) {
+                            potentialBombs.Add(new Arr(w - 1, h - 1));
+                        }
+                        if (mineField[w - 1, h - 1].state > 0) {
+                            flagedTiles++;
+                        }
+                    }
+                    if (h < height - 1) {
+                        if (!mineField[w - 1, h + 1].isPressed && mineField[w - 1, h + 1].state == 0) {
+                            potentialBombs.Add(new Arr(w - 1, h + 1));
+                        }
+                        if (mineField[w - 1, h + 1].state > 0) {
+                            flagedTiles++;
+                        }
+                    }
+                }
+                if (w < width - 1) {
+                    if (!mineField[w + 1, h].isPressed && mineField[w + 1, h].state == 0) {
+                        potentialBombs.Add(new Arr(w + 1, h));
+                    }
+                    if (mineField[w + 1, h].state > 0) {
+                        flagedTiles++;
+                    }
+                    if (h >= 1) {
+                        if (!mineField[w + 1, h - 1].isPressed && mineField[w + 1, h - 1].state == 0) {
+                            potentialBombs.Add(new Arr(w + 1, h - 1));
+                        }
+                        if (mineField[w + 1, h - 1].state > 0) {
+                            flagedTiles++;
+                        }
+                    }
+                    if (h < height - 1) {
+                        if (!mineField[w + 1, h + 1].isPressed && mineField[w + 1, h + 1].state == 0) {
+                            potentialBombs.Add(new Arr(w + 1, h + 1));
+                        }
+                        if (mineField[w + 1, h + 1].state > 0) {
+                            flagedTiles++;
+                        }
+                    }
+                }
+                if (h >= 1) {
+                    if (!mineField[w, h - 1].isPressed && mineField[w, h - 1].state == 0) {
+                        potentialBombs.Add(new Arr(w, h - 1));
+                    }
+                    if (mineField[w, h - 1].state > 0) {
+                        flagedTiles++;
+                    }
+                }
+                if (h < height - 1) {
+                    if (!mineField[w, h + 1].isPressed && mineField[w, h + 1].state == 0) {
+                        potentialBombs.Add(new Arr(w, h + 1));
+                    }
+                    if (mineField[w, h + 1].state > 0) {
+                        flagedTiles++;
+                    }
+                }
+                if (flagedTiles.Equals(amountOBombs)) {
+                    foreach (Arr ar in potentialBombs) {
+                        if (!Arr.Contain(possibleHints, ar) && !mineField[ar.w, ar.h].isPressed){
+                            possibleHints.Add(ar);
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+        foreach(Arr ar in hidenFlags) {
+            mineField[ar.w, ar.h].state = 0;
+        }
+        //if list is not empty pick a random number and return
+        if (possibleHints.Count > 0) {
+            PickRandomClue(possibleHints);
+        }
+        else {
+            Debug.Log("no guaranted safe tiles left");
+        }
+    }
+    void PickRandomClue(List<Arr> possibleHints) {
+       
+        int randomIndex = Random.RandomRange(0, possibleHints.Count - 1);
+        currentHint = new Arr(possibleHints[randomIndex].w, possibleHints[randomIndex].h);
+        possibleHints.RemoveAt(randomIndex);
+        mineField[currentHint.w, currentHint.h].tileGO.GetComponent<SpriteRenderer>().color = (((currentHint.w + currentHint.h) % 2) == 0) ? hintColor1 : hintColor2;
+    }
+
     public struct Tile
     {
         public bool containMine;
@@ -349,9 +556,20 @@ public class MapGenerator : MonoBehaviour
 
     public class Arr
     {
-       public Arr(int w, int h) {
+        public Arr(int w, int h) {
             this.w = w;
             this.h = h;
+        }
+        public Arr(Arr arr) {
+            w = arr.w;
+            h = arr.h;
+        }
+        public static bool Contain(List<Arr> list, Arr arr) {
+            for (int i = 0; i < list.Count; i++) {
+                if (list[i].w.Equals(arr.w) && list[i].h.Equals(arr.h))
+                    return true;
+            }
+            return false;
         }
 
         public int w;
